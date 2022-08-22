87cf305905e8e79cbd182cbc3ab35545aba5a581
moonstal1506 <moonstal1506@gmail.com>
2022-08-23 00:51:13 +0900

    feat: posts 등록

---
+++ src/main/java/com/moonstal/board/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.moonstal.board.service.posts;
+
+import com.moonstal.board.domain.posts.PostsRepository;
+import com.moonstal.board.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
