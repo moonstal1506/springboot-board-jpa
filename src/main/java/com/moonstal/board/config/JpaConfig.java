b4e708c02469ef0567a27f5d32fd8494f61d208a
moonstal1506 <moonstal1506@gmail.com>
2022-08-23 20:54:19 +0900

    test: 테스트에 시큐리티 적용

---
+++ src/main/java/com/moonstal/board/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package com.moonstal.board.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {}
--- src/test/java/com/moonstal/board/web/HelloControllerTest.java
+++ src/test/java/com/moonstal/board/web/HelloControllerTest.java
@@ -1,19 +1,28 @@
 package com.moonstal.board.web;
 
+import com.moonstal.board.config.auth.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(HelloController.class)
+@WebMvcTest(value = HelloController.class, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+        classes = SecurityConfig.class)
+})
 public class HelloControllerTest {
+
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER")
     @Test
     void hello가_리턴된다() throws Exception {
         //given
@@ -26,6 +35,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     void helloDto가_리턴된다() throws Exception {
         String name = "hello";
