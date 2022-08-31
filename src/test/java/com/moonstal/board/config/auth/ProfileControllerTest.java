5ea8bb45eb9143f569be821a1a735505642d8116
moonstal1506 <moonstal1506@gmail.com>
2022-09-01 01:07:04 +0900

    chore: 파일명 변경

--- src/test/java/com/moonstal/board/config/auth/ProfileControllerTest.java
+++ src/test/java/com/moonstal/board/config/auth/ProfileControllerTest.java
@@ -7,10 +7,8 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
